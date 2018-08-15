3d59d31f61a6b4d31839c03c23e230a93296920a
KURO <kurocode404@gmail.com>
2018-08-15 22:36:41 +0200

    UnpaX.py

---
+++ unpax.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                           
+#          ~ UnpaX ~                            
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# CoDeD: bY KURO-CODE
+# DaTe: 12/12/2017
+# Dev: Python
+# Ver: 1
+#
+#~~~~~~~~~~~ INFO ~~~~~~~~~~~~
+#
+#   Cracker Zip & Rar files...
+#
+#     *** Requirements ***
+#  - Zip
+#  - Unrar
+#
+#*****************************
+
+import os, time, sys, signal, subprocess
+import itertools
+import zipfile
+import rarfile
+
+#~~~~ Keybord interruption ~~~~
+def signal_handler(signal, frame):
+		KURO()
+		LOGO()
+		print('\033[1;m [\033[1;31mX\033[1;m] You pResSsΣd Ctrl+C!')
+		time.sleep(2)
+		KURO()
+		subprocess.call(['pkill', '-f', 'python'])
+signal.signal(signal.SIGINT, signal_handler)
+
+#~~~ Function KURO ~~~~
+def KURO():
+	if os.name == 'nt':
+            os.system('cls')
+	else:
+            os.system('clear')
+
+#~~~~ L O G O ~~~~
+def LOGO():
+	KURO()
+
+	print """\033[1;34m
+  ...    ::::::.    :::.::::::::::. :::.     \033[1;31m .,::      .:\033[1;34m
+  ;;     ;;;`;;;;,  `;;; `;;;```.;;;;;`;;    \033[1;31m `;;;,  .,;; \033[1;34m
+ [['     [[[  [[[[[. '[[  `]]nnn]]',[[ '[[,  \033[1;31m   '[[,,[['  \033[1;34m
+ $$      $$$  $$$ "Y$c$$   $$$""  c$$$cc$$$c \033[1;31m    Y$$$P    \033[1;m
+ 88    .d888  888    Y88   888o    888   888,\033[1;31m  oP"``"Yo,  \033[1;m
+  "YmmMMMM""  MMM     YM   YMMMb   YMM   ""`\033[1;31m,m"       "Mm,\033[0;32m
+──────────[\033[1;mUnpa\033[1;31mX\033[1;m ZIP\033[1;33m &\033[1;m R\033[1;34mΛ\033[1;mR CRΛCKER BY \033[1;34mK\033[1;mURO-CØD\033[1;34mΣ\033[0;32m]\033[0;32m───────────\033[1;m
+"""
+
+
+#~~~~ M E N U ~~~~
+def MENU():
+	KURO()
+	LOGO()
+	print """
+	  ╔═\033[1;32m ØpTiOnS\033[1;m ══════════════════╗
+	  ║                            ║
+	  ║    1)\033[0;30m...\033[1;mWorDlisT CreΛtØr   ║
+	  ║    2)\033[0;30m...\033[1;mCrΛck Zip filΣ     ║
+	  ║    3)\033[0;30m...\033[1;mCrΛck Rar fiLe     ║
+	  ║\033[1;31m    0\033[1;m)\033[0;30m...\033[1;31mΣ\033[1;mxiT               ║
+	  ║                            ║
+	  ╚════════════════════════════╝
+	"""
+	unpax = raw_input(" SeLEcT: ")
+	if unpax == '1':
+		KURO()
+		LOGO()
+		print " *** WorLisT CreΛtØr ***\n"
+		time.sleep(1)
+		print " ~ ΣnTer ChΛrs ~\n\033[1;32m-----------------\n\033[1;m"
+		chrs=raw_input(' chΛrs: ')
+		Min=int(raw_input(' Min: '))
+		Max=int(raw_input(' Max: '))
+		Name=raw_input(' NamΣ fiLe: ')
+		print "\033[1;m FilΣ cReΛteD..."
+		KURO()
+		LOGO()
+
+		N_file=Name+'.txt'
+		timer=time.asctime()
+		start=time.time()
+		end=time.time()
+		time.x=end-start
+		ln=len(chrs)
+		TXT=[]
+
+		file= open(N_file, 'a')
+		for x in xrange(Min,Max+1):
+			meta=ln**x
+			TXT.append(meta)
+			TXT_line=sum(TXT)
+			for xs in itertools.product(chrs, repeat=x):
+				file.write(''.join(xs)+'\n')
+		file.close()
+		KURO()
+		LOGO()
+		sys.stdout.write("\033[1;32m\r FiNisHΣd\n")
+		time.sleep(3)
+		KURO()
+		LOGO()
+		print '\t\033[1;32m NamΣ FilΣ   :',N_file
+		print '\t\033[1;33m ChΛrs Min   :',Min
+		print '\t\033[1;33m CHarS MΛx   :',Max
+		print '\t\033[1;m cHaRs       :',chrs
+		print '\t\033[1;34m TotΛl LiNΣs :',TXT_line
+		print '\t TimΣ        :',time.x
+		print ""
+		raw_input("\033[1;mPreSs Enter, rΣtuRn TØ mΛiN MenU...")
+		MENU()
+	elif unpax == '2':
+		KURO()
+		LOGO()
+		zipFile = raw_input(" ZipFilΣ: ")
+		if os.path.isfile(zipFile):
+			KURO()
+			LOGO()
+			print " File ΣxIsT!"
+			time.sleep(2)
+			KURO()
+		else:
+			KURO()
+			LOGO()
+			print " FilΣ nØt ΣxIsT!"
+			time.sleep(2)
+			MENU()
+		LOGO()
+		File = raw_input(" List: ")
+		if os.path.isfile(File):
+			KURO()
+			LOGO()
+			print " FiLe ΣxIsT!"
+			time.sleep(2)
+			KURO()
+		else:
+			KURO()
+			LOGO()
+			print " FilΣ nØt ΣxIsT!"
+			time.sleep(2)
+			MENU()
+
+		zipFile = zipfile.ZipFile(zipFile)
+		password = []
+		LOGO()
+		print " CrΛcK... pLeΛsΣ wΛiT..."
+
+		with open(File, 'r') as f:
+			for line in f.readlines():
+				password = line.strip('\n')
+
+				try:
+					zipFile.extractall(pwd=password)
+					KURO()
+					LOGO()
+					print "[+]\033[1;m PΛsswØrD FoUnD:\033[1;32m %s \033[1;m\n" % password
+					raw_input(" PreSs Enter, rΣtuRn TØ mΛiN MenU...")
+					MENU()
+				except:
+					pass	
+
+	elif unpax == '3':
+		KURO()
+		LOGO()
+		rarFile = raw_input(" RarFilΣ: ")
+		if os.path.isfile(rarFile):
+			KURO()
+			LOGO()
+			print " FiLe ΣxIsT!"
+			time.sleep(1)
+			KURO()
+		else:
+			KURO()
+			LOGO()
+			print " FilΣ nØt ΣxIsT!"
+			time.sleep(1)
+			MENU()
+		LOGO()
+		File = raw_input(" List: ")
+		if os.path.isfile(File):
+			KURO()
+			LOGO()
+			print " FiLe ΣxIsT!"
+			time.sleep(1)
+			KURO()
+		else:
+			KURO()
+			LOGO()
+			print " FilΣ nØt ΣxIsT!"
+			time.sleep(1)
+			MENU()
+
+		rarFile = rarfile.RarFile(rarFile)
+		password = []
+		KURO()
+		LOGO()
+		print " CrΛcK... pLeΛsΣ wΛiT..."
+
+		with open(File, 'r') as f:
+			for line in f.readlines():
+				password = line.strip('\n')
+				try:
+					rarFile.extractall(pwd=password)
+					KURO()
+					LOGO()
+					print "[+]\033[1;m PΛsswØrD FoUnD: \033[1;32m%s \033[1;m\n" % password
+					raw_input(" PreSs Enter, rΣtuRn TØ mΛiN MenU...")
+					MENU()
+				except:
+					pass
+
+	elif unpax == '0':
+		KURO()
+		LOGO()
+		time.sleep(1)
+		print "\033[1;m ThΛnkS fØr UsiNg UnpaX\033[1;m"
+		time.sleep(2)
+		print
+		print "\033[1;m[\033[1;31mX\033[1;m]...\033[1;32mClØsiNg"
+		time.sleep(1)
+		KURO()
+		subprocess.call(['pkill', '-f', 'python'])
+	else:
+		KURO()
+		LOGO()
+		print " [\033[1;33m***\033[1;31mΣRROR\033[1;33m***\033[1;m]"
+		time.sleep(2)
+		MENU()
+	
+MENU()
